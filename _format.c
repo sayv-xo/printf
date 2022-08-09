cc370cb4fa4f0fb857e1541c9b74945a2197ab46
sayv-xo <savegawugah01@gmail.com>
2022-08-09 17:38:26 +0000

    Handling unknown specifier

--- _format.c
+++ _format.c
@@ -35,6 +35,11 @@ int _format(const char *format, va_list fmt)
 				length += spec[1].f(fmt);
 			else if (format[j] == '%')
 				length += _putchar(format[j]);
+			else
+			{
+				length += _putchar(format[j - 1]);
+				length += _putchar(format[j]);
+			}
 		}
 		j++;
 	}
